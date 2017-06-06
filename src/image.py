6f58e0949a58efb0478674f5d031ed1e3685dea0
Brian Hicks <brianhicks@DN525fdb.SUNet>
2017-06-05 21:53:25 -0700

    Implemented NN...not effective :(

--- src/image.py
+++ src/image.py
@@ -11,56 +11,276 @@ RED_LUMA = 0.299
 GREEN_LUMA = 0.587
 BLUE_LUMA = 0.114
 
-def readImage(filename):
-    imageArray = misc.imread(filename)
-    return Image(imageArray[:, :, RED_INDEX:BLUE_INDEX + 1])
-
 class Image(object):
 
-    def __init__(self, imageArray):
-        self.image = imageArray
+    def __init__(self, image):
+        if isinstance(image, np.ndarray):
+            self.image = image.astype(float)
+        elif isinstance(image, Image):
+            self.image = np.array(image.image, copy = True)
+        elif isinstance(image, str):
+            self.image = misc.imread(image)[:, :, RED_INDEX : BLUE_INDEX + 1].astype(float)
 
-    def getHeight(self):
-        return self.image.shape[1]
-
-    def getWidth(self):
+    @property
+    def height(self):
         return self.image.shape[0]
 
-    def getRGB(self, x, y):
-        red = self.image[x, y, RED_INDEX]
-        green = self.image[x, y, GREEN_INDEX]
-        blue = self.image[x, y, BLUE_INDEX]
+    @property
+    def width(self):
+        return self.image.shape[1]
 
-        return (red, green, blue)
+    @property
+    def size(self):
+        return (self.height, self.width)
 
-    def getGrayscaleImage(self):
-        weightMatrix = np.array([RED_LUMA, GREEN_LUMA, BLUE_LUMA])
-        weightMatrix = np.reshape(weightMatrix, [1, 1, weightMatrix.shape[0]])
+    def resize(self, new_width, new_height, in_place = False):
+        new_image = misc.imresize(self.image, (height, width))
+        if not in_place:
+            return Image(new_image)
+        else:
+            self.image = new_image.astype(float)
 
-        weightedImage = self.image * weightMatrix
-        grayscale = np.sum(weightedImage, axis=2)
+    def crop(self, row, col, new_height, new_width, in_place = False):
+        new_image = self.image[row : row + new_height, col : col + new_width]
 
-        grayscale = np.dstack((grayscale, grayscale, grayscale))
-        return Image(grayscale)
+        if not in_place:
+            return Image(new_image)
+        else:
+            self.image = new_image.astype(float)
 
-    def getGrayscaleHistogram(self, axis=1):
-        grayscaleImage = self.getGrayscaleImage()
-        return np.mean(grayscaleImage.image[:, :, RED_INDEX], axis = axis)
+    def add_noise(self, intensity, monochromatic = True, in_place = False):
+        if monochromatic:
+            third_dimension = 1
+        else:
+            third_dimension = 3
 
-    def resize(self, width, height):
-        newImage = misc.imresize(self.image, (height, width))
-        return Image(newImage)
+        noise_layer = 2 * intensity * np.random.rand(self.height, self.width, third_dimension) - intensity
+        new_image = self.image + noise_layer
+        new_image = np.clip(new_image, 0, 255)
 
-    def saveImage(self, filename):
-        image = misc.toimage(self.image, cmin=0, cmax=255, mode="RGB", channel_axis=2)
+        if not in_place:
+            return Image(new_image)
+        else:
+            self.image = new_image
+
+    def convert_to_grayscale(self, in_place = False):
+        weight_matrix = np.array([RED_LUMA, GREEN_LUMA, BLUE_LUMA])
+        weight_matrix = np.reshape(weight_matrix, [1, 1, weight_matrix.shape[0]])
+
+        weighted_image = self.image * weight_matrix
+        grayscale_image = np.sum(weighted_image, axis = 2)
+
+        new_image = np.dstack((grayscale_image, grayscale_image, grayscale_image))
+
+        if not in_place:
+            return Image(new_image)
+        else:
+            self.image = new_image.astype(float)
+
+    def darken(self, multiplier, in_place = False):
+        if multiplier > 100 or multiplier < 0:
+            raise IndexError("Multipler must fall in the closed interval [0, 100]")
+
+        multiplier /= 100.0
+        multiplier = 1 - multiplier
+
+        new_image = multiplier * self.image
+
+        if not in_place:
+            return Image(new_image)
+        else:
+            self.image = new_image.astype(float)
+
+    def brighten(self, multiplier, in_place = False):
+        if multiplier > 100 or multiplier < 0:
+            raise IndexError("Multipler must fall in the closed interval [0, 100]")
+
+        multiplier = (100 - multiplier) / 100.0
+
+        new_image = 255 - self.image
+        new_image = multiplier * new_image
+        new_image = 255 - new_image
+
+        if not in_place:
+            return Image(new_image)
+        else:
+            self.image = new_image
+
+    # region is assumed to be a 4-tuple of
+    # the form (row_min, col_min, row_max, col_max)
+    #
+    # Note: the region is the region of the hog,
+    # not the region that is required for the gradient
+    # however, the region cannot include the extremes
+    # in any direction
+    def calculate_hog(self, region, nbins):
+        row_min, col_min, row_max, col_max = region
+
+        if row_min < 1 or row_max > self.height - 1:
+            raise IndexError()
+        if col_min < 1 or col_max > self.width - 1:
+            raise IndexError()
+
+        # get gradients for the region
+        angles, magnitudes = self.__compute_gradients__(region)
+
+        # determine the histogram of gradients
+        histogram = np.zeros([nbins], dtype = float)
+
+        width = angles.shape[1]
+        height = angles.shape[0]
+
+        bin_width = float(180)/nbins
+        midpoint = float(bin_width) / 2
+
+        for i in range(height):
+            for j in range(width):
+
+                angle = angles[i, j]
+                magnitude = magnitudes[i, j]
+
+                if angle < midpoint:
+                    bin_above = 0
+                    bin_below = nbins - 1
+
+                    angle_above = midpoint
+                    angle_below = -midpoint
+
+                    histogram[bin_below] += magnitude * np.abs(angle - angle_above) / (180 / nbins)
+                    histogram[bin_above] += magnitude * np.abs(angle - angle_below) / (180 / nbins)
+                    break
+
+                if angle >= 180 - midpoint:
+                    bin_above = 0
+                    bin_below = nbins - 1
+
+                    angle_above = 180 + midpoint
+                    angle_below = 180 - midpoint
+
+                    histogram[bin_below] += magnitude * np.abs(angle - angle_above) / (180 / nbins)
+                    histogram[bin_above] += magnitude * np.abs(angle - angle_below) / (180 / nbins)
+                    break
+
+                for k in range(nbins - 1):
+                    bin_below = k
+                    bin_above = k + 1
+
+                    angle_below = bin_below * bin_width + midpoint
+                    angle_above = bin_above * bin_width + midpoint
+
+                    if angle_below <= angle and angle_above >= angle:
+                        histogram[bin_below] += magnitude * np.abs(angle - angle_above) / (180 / nbins)
+                        histogram[bin_above] += magnitude * np.abs(angle - angle_below) / (180 / nbins)
+                        break
+
+        return histogram
+
+
+    def save_image(self, filename):
+        image = misc.toimage(self.image, cmin = 0, cmax = 255, mode = "RGB", channel_axis = 2)
         image.save(filename)
 
-    def displayImage(self):
-        # rescale
-        decArray = self.image/255
-        plt.imshow(decArray)
+    def display_image(self):
+        image = misc.toimage(self.image, cmin = 0, cmax = 255, mode = "RGB", channel_axis = 2)
+        plt.imshow(image)
         plt.show()
 
-    def extractSubImage(self, x, y, width, height):
-        subimage = self.image[y:y+height, x:x+width, :]
-        return Image(subimage)
+    # region is assumed to be passed in as a
+    # 4-tuple of the form (row_min, col_min, row_max, col_max)
+    def __compute_gradients__(self, region):
+        row_min, col_min, row_max, col_max = region
+        if row_min < 1 or row_max > self.height - 1:
+            raise IndexError()
+        if col_min < 1 or col_max > self.width - 1:
+            raise IndexError()
+
+        height = row_max - row_min
+        width = col_max - col_min
+
+        if height < 1:
+            raise IndexError()
+        if width < 1:
+            raise IndexError()
+
+        angles = np.zeros([height, width], dtype=float)
+        magnitudes = np.zeros([height, width], dtype=float)
+
+        for row in range(row_min, row_max):
+            for col in range(col_min, col_max):
+                above = self.image[row - 1, col, 0]
+                below = self.image[row + 1, col, 0]
+
+                left = self.image[row, col - 1, 0]
+                right = self.image[row, col + 1, 0]
+
+                dx = right - left
+                dy = below - above
+
+                if dx == 0:
+                    angle = 90
+                else:
+                    angle = np.arctan(dy / dx) * 180 / np.pi
+
+                magnitude = np.sqrt(dx ** 2 + dy ** 2)
+
+
+                angles[row - row_min, col - col_min] = angle
+                magnitudes[row - row_min, col - col_min] = magnitude
+
+        return angles, magnitudes
+
+    def __setitem__(self, args, new_value):
+        row = None
+        col = None
+        color = None
+
+        if len(args) == 2:
+            row, col = args
+        else:
+            row, col, color = args
+
+        if row < 0 or row >= self.height:
+            raise IndexError()
+        if col < 0 or col >= self.width:
+            raise IndexError()
+        if color is not None and (color < 0 or color > BLUE_INDEX):
+            raise IndexError()
+
+        if color is None:
+            self.image[row, col] = new_value
+        else:
+            self.image[row, col, color] = new_value
+
+    def __getitem__(self, args):
+        row = None
+        col = None
+        color = None
+
+        if len(args) == 2:
+            row, col = args
+        else:
+            row, col, color = args
+
+        if row < 0 or row >= self.height:
+            raise IndexError()
+        if col < 0 or col >= self.width:
+            raise IndexError()
+        if color is not None and (color < 0 or color > BLUE_INDEX):
+            raise IndexError()
+
+        if color is None:
+            return self.image[row, col, :]
+        else:
+            return self.image[row, col, color]
+
+        if row < 0 or row >= self.height:
+            raise IndexError()
+        if col < 0 or y >= self.width:
+            raise IndexError()
+        if color is not None and (color < 0 or color > BLUE_INDEX):
+            raise IndexError()
+
+        if color is None:
+            return self.image[row, col, :]
+        else:
+            return self.image[row, col, color]
